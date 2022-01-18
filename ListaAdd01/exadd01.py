00afd7758d5934ff433c0d263658ef7335870b37
PatrickFRR <patrick_ribeiro@ymail.com>
2022-01-18 08:17:58 -0300

    add lista 01 e lista adicional 01

---
+++ ListaAdd01/exadd01.py
@@ -0,0 +1,6 @@
+name = input('Digite o nome do cliente: ')
+day = input('Digite o dia de vencimento: ')
+month = input('Digite o mês de vencimento: ')
+value = input('Digite o valor da fatura: ')
+print('Olá, {} \nA sua fatura com vencimento em {} de {} no valor de R$ {} está fechada'.format(name, day, month, value))
+
