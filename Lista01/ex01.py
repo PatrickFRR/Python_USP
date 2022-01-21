37cf07d5e3caaeea2d9adb06ffb74f00da5110bd
Patrick F R Ribeiro <87321768+PatrickFRR@users.noreply.github.com>
2022-01-21 12:31:22 -0300

    Add revision complite files
    Add revision complete files Lista01 and ListaAdd01 + Lista02 and ListaAdd02

--- Lista01/ex01.py
+++ Lista01/ex01.py
@@ -1,4 +1,4 @@
-n = int(input('Digite o valor correspondente ao lado de um quadrado: '))
+n = int(input('Digite o valor correspondente ao lado de um quadrado:'))
 p = n * 4 #calculo do perimetro
 a = n * n #calculo da area
-print('perímetro:{} - área: {}'.format(p, a))
+print("perímetro:{}-área:{}".format(p, a))
--- ListaAdd01/exadd01.py
+++ ListaAdd01/exadd01.py
@@ -2,5 +2,6 @@ name = input('Digite o nome do cliente: ')
 day = input('Digite o dia de vencimento: ')
 month = input('Digite o mês de vencimento: ')
 value = input('Digite o valor da fatura: ')
-print('Olá, {} \nA sua fatura com vencimento em {} de {} no valor de R$ {} está fechada'.format(name, day, month, value))
+print('Olá, {}'.format(name))
+print('A sua fatura com vencimento em {} de {} no valor de R$ {} está fechada.'.format(day, month, value))
 
